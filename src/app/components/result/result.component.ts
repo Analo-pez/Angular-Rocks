de47fb94b669861e97e35ef72b8717b05435f7c8
Analo-pez <ana.ts.lopez@gmail.com>
2021-01-19 14:02:41 +0100

    add delete button

--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BandsDataService } from '../../services/bands-data.service';
+
 
 @Component({
   selector: 'app-result',
@@ -9,9 +12,18 @@ export class ResultComponent implements OnInit {
 
   @Input() band: any = {};
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: BandsDataService
+  ) {
+    this.route.params.subscribe(params => {
+      this.band = this.service.getId(params['id'])
+    })
+  }
 
   ngOnInit(): void {
   }
-
+  deleteCard() {
+    console.log('delete')
+  }
 }
